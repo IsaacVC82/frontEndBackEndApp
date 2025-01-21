835546f08a9ed16e65344a4fce81d6810273ecf3
Isaac Vargas <89848120+IsaacVC82@users.noreply.github.com>
2025-01-21 13:01:59 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,33 +1,34 @@
+
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); 
+
 
 const { connectDB } = require("./config/db");
 
 const app = express();
 
-app.use(cors({
-  origin: "https://", 
-  credentials: true, 
-}));
+
+const allowedOrigins = ['https://limegreen-gorilla-284669.hostingersite.com/create-todo']; 
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+// Conexión a la base de datos
+connectDB(); // Esta función conecta a MySQL
 
 // Rutas
 const todo = require("./routes/todo");
 
-// Conectar a la base de datos
-connectDB(); // Conecta a MySQL
-
-// Middleware
+// Inicializa middleware
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("Server up and running"));
 
-// Usar rutas
+// Utiliza las rutas
 app.use("/api/todo", todo);
 
-// Establecer puerto
+// Puerto de la aplicación
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
