1d215efac746fa6bebcb4c86cd75f2fa977ca362
Isaac Vargas <89848120+IsaacVC82@users.noreply.github.com>
2025-01-22 10:57:31 -0600

    Update createTodo.jsx

--- client/src/components/createTodo.jsx
+++ client/src/components/createTodo.jsx
@@ -37,7 +37,7 @@ export function CreateTodo() {
         };
 
         axios
-            .post(`${process.env.REACT_APP_API_URL}/todo`, todo)
+            .post(`${process.env.REACT_APP_API_URL}todo`, todo)
             .then((res) => {
                 setData({
                     title: "",
