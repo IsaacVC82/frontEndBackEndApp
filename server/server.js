68aa112eb816bbfc968d3e404e8e2d7090260ba2
IsaacVC82 <drawcommissions@gmail.com>
2025-02-02 12:46:00 -0600

    otro commit para Render

--- server/server.js
+++ server/server.js
@@ -10,16 +10,32 @@ dotenv.config();
 // Crear la aplicación de Express
 const app = express();
 
-// Importar las rutas
-const todoRoutes = require('./routes/todo');
+// Usar Helmet con configuración personalizada de CSP
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      imgSrc: ["'self'", "https://frontendbackendapp.onrender.com"],  // Permitir imágenes
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],  // Permitir estilos en línea
+      connectSrc: ["'self'"],
+      fontSrc: ["'self'"],
+      objectSrc: ["'none'"],
+      upgradeInsecureRequests: [],
+    }
+  })
+);
 
 // Middlewares
-app.use(helmet());
+app.use(helmet());  // Usar Helmet para otras configuraciones de seguridad
 app.use(cors());
 app.use(express.json());
 
+// Importar las rutas
+const todoRoutes = require('./routes/todo');
+
 // Usar las rutas
-app.use('/api', todoRoutes);  
+app.use('/api', todoRoutes);
 
 // Conectar a la base de datos
 connectDB();  
@@ -29,3 +45,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
