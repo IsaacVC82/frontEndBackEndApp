a209cf29358972889627c2e08e2449a0ddd3c217
IsaacVC82 <drawcommissions@gmail.com>
2025-02-04 08:25:23 -0600

    Commit para corregir rutas

--- server/server.js
+++ server/server.js
@@ -12,10 +12,10 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 app.use('/api/todos', require('./routes/todo'));
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 // Puerto en el que corre el servidor
