0f331e0ec9cbf9ca5f0f094253656d89c4299ab7
IsaacVC82 <drawcommissions@gmail.com>
2025-02-14 23:24:09 -0600

    Deploy correction

--- server/server.js
+++ server/server.js
@@ -8,8 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+    origin: "https://isaacvc82.github.io/frontendbackendapp", 
+    credentials: true,
+  }));app.use(express.json());
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
