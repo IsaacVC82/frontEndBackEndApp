8aaeeb5e01b6949b12714b23c464672808635d89
IsaacVC82 <drawcommissions@gmail.com>
2025-03-09 17:42:47 -0600

    Corrección en la creación de tareas

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
-const todoSchema = new mongoose.Schema({
+// Verifica si el modelo ya está definido antes de crearlo
+const Todo = mongoose.models.Todo || mongoose.model('Todo', new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: false },
   date: { type: Date, required: false },
   priority: { type: String, required: false },
   done: { type: Boolean, default: false },
-  userId: { type: String, required: true }, 
-});
-
-const Todo = mongoose.model("Todo", todoSchema);
+  userId: { type: String, required: true }, // Este campo identifica al usuario
+}));
 
 module.exports = Todo;
+
 
 
 
